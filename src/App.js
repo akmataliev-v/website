78464495b59475190bc260e0684abcc63152cb43
Baiastan <bsbaha195@gmail.com>
2023-07-25 16:10:24 +0600

    add css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { useState } from "react";
-import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import AppHeader from "./components/header";
 
 function App() {
   return (
     <>
-      <Button variant="dark">Primary</Button>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img
-          variant="top"
-          src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
-        />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <header id="header">
+        <AppHeader />
+      </header>
     </>
   );
 }
