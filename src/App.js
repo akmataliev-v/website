705c9ff13d4f0c10918348734587007bf3cc9d95
Baiastan <bsbaha195@gmail.com>
2023-07-26 16:02:10 +0600

    added services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AppHeader from "./components/header";
+import AppHero from "./components/hero";
+import AppAbout from "./components/about";
+import AppServices from "./components/services";
 
 function App() {
   return (
@@ -11,6 +14,11 @@ function App() {
       <header id="header">
         <AppHeader />
       </header>
+      <main>
+        <AppHero />
+        <AppAbout />
+        <AppServices />
+      </main>
     </>
   );
 }
